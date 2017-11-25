97af167794f7031a78fa6f2ef0199d9f246ae36d
skylouis <louis.dumas.m1@polytechnique.edu>
2017-11-25 22:08:35 +0100

    About
    ouverture de la page About depui le logo du menu

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {WelcomePage} from '../welcome/welcome';
 
 @Component({
     selector: 'page-about',
@@ -9,7 +10,7 @@ import { NavController } from 'ionic-angular';
 export class AboutPage {
 
   constructor(public navCtrl: NavController) {
-
+    
   }
 
 }
